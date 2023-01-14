86ad0998b3220fa74b9c7522c3bfd3030d6878a8
Renan Rodrigues Machado <renanrodriguesmachado2@gmail.com>
2023-01-14 19:58:50 -0300

    wip(results): criada página results

--- src/app/pages/results/results.module.ts
+++ src/app/pages/results/results.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ResultsComponent } from "./results.component";
+import {CardModule} from 'primeng/card';
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
-  imports:[CommonModule],
+  imports:[CommonModule, CardModule,ButtonModule],
   declarations:[ResultsComponent],
   exports:[ResultsComponent]
 })
