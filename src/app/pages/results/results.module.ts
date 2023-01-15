f2358c7c233ad4c843560fad450f26e705cdfb11
Renan Rodrigues Machado <renanrodriguesmachado2@gmail.com>
2023-01-14 23:00:23 -0300

    feat: vinculada página home a de results

--- src/app/pages/results/results.module.ts
+++ src/app/pages/results/results.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 import { ResultsComponent } from "./results.component";
 import {CardModule} from 'primeng/card';
 import { ButtonModule } from "primeng/button";
+import { InputTextModule } from "primeng/inputtext";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports:[CommonModule, CardModule,ButtonModule],
+  imports:[CommonModule, CardModule,ButtonModule,InputTextModule,RouterModule],
   declarations:[ResultsComponent],
   exports:[ResultsComponent]
 })
