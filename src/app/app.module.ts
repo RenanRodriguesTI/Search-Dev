f30c25ec38adebd26bb1b963933e2b6dd2c25b02
Renan Rodrigues Machado <renanrodriguesmachado2@gmail.com>
2023-01-15 22:40:09 -0300

    deploy: configuração de deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,42 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from '@pages/home/home.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { HttpClientModule } from '@angular/common/http';
+import { LoadingService } from '@services/loading.service';
+import { LoadingComponent } from './shared/components/loading/loading.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChZoHh2OPRYkim2cwR2JUd-pKpV61873E",
+  authDomain: "searchdev-347c8.firebaseapp.com",
+  projectId: "searchdev-347c8",
+  storageBucket: "searchdev-347c8.appspot.com",
+  messagingSenderId: "322329516073",
+  appId: "1:322329516073:web:a64f8fc6d6a8513136a438",
+  measurementId: "G-D1QZXKLM62"
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
     HomeModule,
     AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     NgbModule,
   ],
   exports:[],
-  providers: [],
+  providers: [
+    LoadingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
